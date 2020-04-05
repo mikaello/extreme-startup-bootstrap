7f3089eb012179e00387a92aaedb3155d11e6cca
mikaello <2505178+mikaello@users.noreply.github.com>
2020-04-05 14:20:34 +0200

    init bootstrap for JavaScript and TypeScript

---
+++ index.mjs
@@ -0,0 +1,19 @@
+import * as http from "http";
+import * as querystring from "querystring";
+
+const server = http.createServer((req, res) => {
+  if (req.url.startsWith("/?question=")) {
+    const queryParams = req.url.split("/?")[1];
+
+    console.log(querystring.parse(queryParams).question);
+
+    res.end("Hello world!");
+  }
+
+  res.statusCode = 404;
+  res.end("Unknown parameter");
+});
+
+server.listen(5000, () => {
+  console.log("Server running at http://localhost:5000/");
+});
