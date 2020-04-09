17ed817ea79760845c3fbe68ea5b48d26f746261
mikaello <2505178+mikaello@users.noreply.github.com>
2020-04-09 15:55:06 +0200

    deno: specify where to start editing

--- deno/server.ts
+++ deno/server.ts
@@ -6,10 +6,12 @@ const server = serve({ port });
 console.log(`Server running at ${host}:${port}`);
 
 for await (const req of server) {
-  const searchParams = (new URL(`${host}:${port}${req.url}`)).searchParams;
+  const searchParams = new URL(`${host}:${port}${req.url}`).searchParams;
   const question = searchParams.get("question");
 
   console.log(question);
+
+  /* Insert your parsing code here */
 
   req.respond({ body: "Hello World" });
 }
